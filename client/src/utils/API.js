19397ceda358d2348b5531d26b9e32769d8b498a
Ram Sah <mohan2036@gmail.com>
2020-10-16 00:32:01 -0400

    Added Yelp API backend functionality code

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,25 +1,24 @@
 import axios from "axios";
+require('dotenv').config();
 
-const APIKEY = "i1ziZYVJ3E6Mu7UzIXbE0039lPqhDkRN";
-
-// const BASEURL = "https://sandbox-api.va.gov/oauth2/authorization?
-//                 client_id=0oa1c01m77heEXUZt2p7
-//                 &response_type=code
-//                 &scope=openid profile email patient/DiagnosticReport.read
-//                 &redirect_uri=<yourRedirectURL>
-//                 &state=499f_073a_5094_fd7a_ff7f_0bf0_69e4_adad_2bdf_e3ab_76b9
-//                 &code_challenge_method=S256
-//                 &code_challenge=gNL3Mve3EVRsiFq0H6gfCz8z8IUANboT-eQZgEkXzKw";
-
-
-
-// Export an object with a "search" method  API 
 export default {
-  // search: function(query) {
-  //   return axios.get(BASEURL + query + APIKEY);
-  // }
+  getBusinesses: function () {
+    return axios.get("/api/businesses");
+  }
+}
+
+//Yelp business search API 
+const business = {
+  corsdomain: true,
+  headers: {
+    Authorization: `Bearer -_OSL_H0z4963ePmeNjsWYtotLgHLzIuWTm6x2fVheP1gEJ37L60BvgKmJnYrdL6T2RBB1pYt_j889F5nDc58uJ8wafEClR1rGuCAXOEtaXaFbhSemGseyCCW--IX3Yx
+    ` }
+};
+const bodyParameters = {
+  key: ""
 };
 
-
-{/* const APIKEY = "&api_key=dc6zaTOxFJmzC&limit=20";
-const BASEURL = "https://api.giphy.com/v1/gifs/search?q="; */}
+axios.get(
+  'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=philadelphia',
+  business
+).then(console.log).catch(console.log);
--- server.js
+++ server.js
@@ -3,14 +3,21 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+const routes = require("./routes/api/discountRoute.js");
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.use(passport.initialize()); 
-app.use(passport.session()); 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// app.use(passport.initialize()); 
+// app.use(passport.session()); 
+
+app.use(routes)
 
 
 // Send every request to the React app
@@ -18,6 +25,10 @@ app.use(passport.session());
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+// Connect to mongodb databases
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/vetDiscount", { useNewUrlParser: true, useUnifiedTopology: true })
+
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
