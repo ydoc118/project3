9ce721c5468a60e30199cd84e03f37d779aaf13e
Cody <ydoc118@gmail.com>
2020-10-17 13:07:23 -0400

    added yelp business search

--- client/src/pages/Business.js
+++ client/src/pages/Business.js
@@ -1,26 +1,77 @@
 import React, { useState, useEffect } from "react";
 import {useParams} from "react-router-dom";
 import API from "../utils/API";
+import axios from "axios";
+
+let latitude;
+let longitude;
 
 function Businesses() {
     const [currentBus, setCurrentbus] = useState({});
-    
     const {id}= useParams();
+
     useEffect(() => {
+        getPosition();
         loadBusiness(id);
+        
     });
 
     function loadBusiness(id) {
         API.getBusiness(id)
             .then(res => {
-                console.log(res.data)
-                setCurrentbus(res.data)
+                console.log(res.data.business)
+                setCurrentbus(res.data.business)
             })
+    };
+
+    function getPosition() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(showPosition);
+          //console.log(showPosition);
+        }
+        function showPosition(position) {
+          console.log(
+            "Latitude: " +
+              position.coords.latitude +
+              " " +
+              "Longitude: " +
+              position.coords.longitude
+          );
+          latitude = position.coords.latitude;
+          longitude = position.coords.longitude;
+        }
     }
+      
+    function yelpBusiness(latitude, longitude) {
+        const queryUrl='https://cors-anywhere.herokuapp.com/api.yelp.com/v3/businesses/search';
+      
+        axios.get(queryUrl, {
+          headers: {
+            Authorization: `Bearer 9AvFmvbitzJpbSlJPaDKPmZbkrL3bKKfqklkwvjYCPZ1xXVwI76ygHd3MJeM9Lglb9kKYbcgfBYFyfN-YFpn6OfsxYKkAe-AP1do3P4Z35iCUh9QflOthO_BaQuLX3Yx` 
+          },
+          params: {
+            term: currentBus,
+            latitude: latitude,
+            longitude: longitude,
+            sort_by: "distance",
+            limit: "3"
+          }
+        })
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
+    yelpBusiness(latitude, longitude)
 
     return(
         <div>
-            <h1>BUSINESS PAGE</h1>
+            <h1>BUSINESS NAME</h1>
+            <h2>Map goes here</h2>
+            <button>Generate QR Code</button>
             
         </div>
     )
