ed68e2fe2496d1d7a4481c2f25fcc66e901563fa
Ram Sah <mohan2036@gmail.com>
2020-10-17 19:17:34 -0400

    Added Basic front pages

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,82 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import "./style.css";
+
+// function Nav() {
+//   return (
+//     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+//       <Link className="navbar-brand" to="/">
+//       VET-DISCOUT
+//       </Link>
+//       <div>
+//         <ul className="navbar-nav">
+
+//         <div class="nav search-row" id="top_menu">
+//         <ul class="nav top-menu">
+//           <li>
+//             <form class="navbar-form">
+//               <input class="form-control" placeholder ="Search" type="text"/>
+//             </form>
+//           </li>
+//         </ul>
+//       </div>
+
+//           <li className="nav-item">
+//             <Link
+//               to="/"
+//               className={
+//                 window.location.pathname === "/" || window.location.pathname === "/about"
+//                   ? "nav-link active"
+//                   : "nav-link"
+//               }
+//             >
+//               About
+//             </Link>
+//           </li>
+
+
+//           <li className="nav-item">
+//             <Link
+//               to="/discover"
+//               className={window.location.pathname === "/discover" ? "nav-link active" : "nav-link"}
+//             >
+//               Categories
+//             </Link>
+//           </li>
+
+
+//           <li className="nav-item">
+//             <Link
+//               to="/search"
+//               className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
+//             >
+//               Businesses
+//             </Link>
+//           </li>
+
+
+//           <li class="nav-item">
+//             <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+//                             <span class="profile-ava">
+//                               {/* <img alt="">src="img/avatar1_small.jpg"</img>  */}
+//                               </span>
+//                             <span class="username">John Doe</span>
+//                             <b class="caret"></b>
+//                             </a>
+//             <ul class="nav-item">
+//               <div class="log-arrow-up"></div>
+//               <li class="eborder-top">
+//               </li>
+//               <li>
+//                 <a href="login.html"><i class="icon_key_alt"></i> Log Out</a>
+//               </li>
+//             </ul>
+//           </li>
+//         </ul>
+//       </div>
+      
+//     </nav>
+//   );
+// }
+
+// export default Nav;
